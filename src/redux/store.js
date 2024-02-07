46b760396f2c47dea2cac05a50a258554fdebf96
Paweł Brzozowski <pawel@brzoza.net>
2024-02-07 13:05:07 +0100

    store & reducer refactoring

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer, filterReducer } from "./reducers";
+import { contactsReducer, filterReducer } from "./reducer";
 
 export const store = configureStore({
   reducer: {
